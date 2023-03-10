73c81c9d2dfb859f60777396ead98c9da8c581e9
Nitin Gupta <nitigupt@adobe.com>
2023-03-10 12:36:46 +0530

    update build

--- spa/src/components/TextField.jsx
+++ spa/src/components/TextField.jsx
@@ -1,6 +1,6 @@
 function TextField(props) {
     return (
-        <div itemtype="urn:fnk:type/component" itemid={generateItemId(props['Name'])} itemScope>
+        <div itemtype="urn:fnk:type/component" itemid={generateItemId(props['Name'])} itemscope>
             <label itemprop="Title" itemtype="text" htmlFor="name">{props['Title']}</label>
             <input type={props['Type']} id="name" name={props['Name']} required minLength="4" maxLength="8" size="10"></input>
         </div>
