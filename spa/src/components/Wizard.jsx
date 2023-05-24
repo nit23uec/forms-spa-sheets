272b867e584534cf0ca8f3d10d4af15a63409dd9
Nitin Gupta <nitigupt@adobe.com>
2023-05-24 19:04:55 +0530

    plain wizard

--- spa/src/components/Wizard.jsx
+++ spa/src/components/Wizard.jsx
@@ -1,25 +1,13 @@
-import {Item, TabList, TabPanels, Tabs} from '@adobe/react-spectrum'
 
 function Wizard(props) {
     return (
-        <Tabs aria-label="History of Ancient Rome">
-        <TabList>
-            <Item key="FoR">Founding of Rome</Item>
-            <Item key="MaR">Monarchy and Republic</Item>
-            <Item key="Emp">Empire</Item>
-        </TabList>
-        <TabPanels>
-            <Item key="FoR">
-            Arma virumque cano, Troiae qui primus ab oris.
-            </Item>
-            <Item key="MaR">
-            Senatus Populusque Romanus.
-            </Item>
-            <Item key="Emp">
-            Alea jacta est.
-            </Item>
-        </TabPanels>
-    </Tabs>
+        <div>
+            <button class="editor">Editor</button>
+            <button class="themes">Themes</button>
+            <div>
+                <button class="integrations">Integrations</button>
+            </div>
+        </div>
    );
 }
 
