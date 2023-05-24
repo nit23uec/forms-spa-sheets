4f174ddf4c7b6a5733824f6f521b01d6b28770e7
Nitin Gupta <nitigupt@adobe.com>
2023-05-24 19:36:20 +0530

    tabs

--- spa/src/components/Wizard.jsx
+++ spa/src/components/Wizard.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 
 function Wizard(props) {
+    const [tab, setTab] = useState(1);
     return (
         <div>
-            <button class="editor">Editor</button>
-            <button class="themes">Themes</button>
             <div>
-                <button class="integrations">Integrations</button>
+                <button onClick={() => setTab(1)}>Tab 1</button>
+                <button onClick={() => setTab(2)}>Tab 2</button>
+                <button onClick={() => setTab(3)}>Tab 3</button>
             </div>
+            {tab == 1 ? (
+            <div class="tab">
+                <iframe src="http://localhost:4502" width="800" height="1000"></iframe>
+            </div>) : (tab == 2 ? 
+            <div className="tab">
+                <input type="text" placeholder="Input for Tab 2"></input>
+            </div> : (tab === 3) ? <div className="tab">
+                <input type="text" placeholder="Input for Tab 3"></input>
+            </div> : '')}
         </div>
    );
 }
